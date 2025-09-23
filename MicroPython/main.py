62731394d4f49b7ae1a60993514a39474f993800
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-23 16:48:16 +0000

    shows temperature in kelvin

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Alexander
+Created on: Sep 2025
+This module shows the temperature of the microbit
 """
 
 from microbit import *
 
+# variable for the temperature
+temperature_of_microbit = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temperature_of_microbit = input.temperature() + 273
+        display.show (' The temperature is: ' + str(temperature_of_microbit) + ' K')
