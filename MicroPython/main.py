008d4e2d35cf6d7ad64c18db2387c76af981c1da
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-24 17:07:01 +0000

    changed name of variable

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,12 +7,12 @@ This module shows the temperature of the microbit in kelvin
 from microbit import *
 
 # variable for the temperature
-temperature_of_microbit = 0
+temperature_of_microbit_in_kelvin = 0
 
 display.clear()
 display.show(Image.HAPPY)
 
 while True:
     if button_a.is_pressed():
-        temperature_of_microbit = input.temperature() + 273
-        display.show("The temperature is: " + str(temperature_of_microbit) + " K")
+        temperature_of_microbit_in_kelvin = input.temperature() + 273
+        display.show("The temperature is: " + str(temperature_of_microbit_in_kelvin) + " K")
